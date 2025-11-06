4e7520dc545df4979fce13876604728cc057afaf
Venkatesh goud <venkateshgoud@Venkateshs-MacBook-Air.local>
2025-11-06 02:11:37 -0800

    server commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+const app = express();
+import cors from 'cors';
+import bodyParser from 'body-parser';
+const PORT = process.env.PORT || 3000;
 dotenv.config();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
 });
@@ -13,5 +27,7 @@ const response = await client.responses.create({
 });
 
 console.log(response.choices[0].message.content);
+
+
 
 
